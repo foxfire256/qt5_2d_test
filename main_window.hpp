cf3fa749aab3c468fcc23dcc1cb66f69ab28b0c4
foxfire <foxfire256@gmail.com>
2018-10-14 09:48:04 -0400

    This shows a plain window now

---
+++ main_window.hpp
@@ -0,0 +1,19 @@
+#ifndef MAIN_WINDOW_HPP
+#define MAIN_WINDOW_HPP
+
+#include <QtWidgets/QMainWindow>
+
+class main_window : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	main_window(QWidget *parent = 0);
+	~main_window();
+
+public slots:
+
+private:
+};
+
+#endif
